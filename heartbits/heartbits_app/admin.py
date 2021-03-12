d0ebd537ba2e73c561a55f171fac57a5ccf540f1
malinleo <mr.malin@list.ru>
2021-03-12 20:38:13 +0700

    Recreate models, change tables relations

--- heartbits/heartbits_app/admin.py
+++ heartbits/heartbits_app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import UserRating, User, Question, Answer, Test
+from .models import User, Question, Answer, Test
 
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(UserRating)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Test)
--- heartbits/heartbits_app/models.py
+++ heartbits/heartbits_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import date
+from datetime import date, datetime
 
 # Create your models here.
 
@@ -18,11 +18,17 @@ STATUS = [
 
 
 class UserRating(models.Model):
-    like = models.PositiveIntegerField('Лайки')
-    value = models.PositiveIntegerField('Рейтинг')
+    id_user_liked = models.ForeignKey('User', verbose_name='Кто поставил', on_delete=models.CASCADE, default=-1)
+
+    def count_likes(self):
+        '''
+        Counts all likes for user
+        :return: likes count
+        '''
+        return UserRating.objects.count()
 
     def __str__(self):
-        return '%s, %s' % (self.like, self.value)
+        return '%s' % self.count_likes()
 
     class Meta:
         verbose_name = 'Рейтинг'
@@ -40,17 +46,20 @@ class User(models.Model):
     user_country = models.CharField('Страна', max_length=100)
     user_city = models.CharField('Город', max_length=100)
     user_description = models.TextField('Описание', max_length=5000)
-    user_image = models.ImageField('Изображение', upload_to='user_photos/')
-    date_register = models.DateField('Дата регистрации', default=date.today)
-    last_online = models.DateTimeField('Заходил последний раз')
+    user_image = models.ImageField('Изображение', upload_to='user_photos/', blank=True)
+    date_register = models.DateField('Дата регистрации', auto_now_add=True)
+    last_online = models.DateTimeField('Заходил последний раз', auto_now_add=True)
     is_blocked = models.BooleanField('Заблокирован', default=False)
-    rating = models.OneToOneField(UserRating, on_delete=models.SET_NULL, null=True)
-    coefficient = models.FloatField('Коэффициент')
+    rating = models.OneToOneField(UserRating, verbose_name='Рейтинг пользователя',
+                                  on_delete=models.SET_NULL, null=True, blank=True)
+    coefficient = models.FloatField('Коэффициент', default=0.0)
+    test = models.OneToOneField('Test', verbose_name='Тест', on_delete=models.SET_NULL, null=True, blank=True)
     user_url = models.SlugField(max_length=160, unique=True)
-    coef_range_min = models.FloatField('Нижняя граница поиска')
-    coef_range_max = models.FloatField('Верхняя граница поиска')
+    coef_range_min = models.FloatField('Нижняя граница поиска', default=0.0)
+    coef_range_max = models.FloatField('Верхняя граница поиска', default=0.0)
     status = models.CharField('Статус', choices=STATUS, max_length=5, null=False)
     display_status = models.BooleanField(default=True)
+
 
     def __str__(self):
         return '%s, %s' % (self.first_name, self.age)
@@ -71,11 +80,10 @@ class Answer(models.Model):
         verbose_name_plural = 'Ответы на вопрос'
 
 
-
 class Question(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(max_length=5000)
-    answers = models.ForeignKey(Answer, verbose_name='Ответы', on_delete=models.CASCADE, default=None, null=True)
+    answers = models.ManyToManyField(Answer, verbose_name='Ответы', default=None, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -90,7 +98,7 @@ class Test(models.Model):
     test_title = models.CharField('Название теста', max_length=300)
     test_description = models.TextField('Описание теста', max_length=5000)
     test_questions = models.ManyToManyField(Question, verbose_name='Вопросы', related_name='test_questions')
-    test_result = models.CharField(max_length=500)
+    test_result = models.CharField(max_length=500, blank=True)
     test_url = models.SlugField(unique=True)
 
     def __str__(self):
