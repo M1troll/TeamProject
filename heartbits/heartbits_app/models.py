9c7505f0de1bd00960172a437bf01b822c283fd0
malinleo <mr.malin@list.ru>
2021-03-13 16:42:47 +0700

    Update models

--- heartbits/heartbits_app/models.py
+++ heartbits/heartbits_app/models.py
@@ -18,24 +18,23 @@ STATUS = [
 
 
 class UserRating(models.Model):
-    id_user_liked = models.ForeignKey('User', verbose_name='Кто поставил', on_delete=models.CASCADE, default=-1)
+    """User Rating Model"""
+    user = models.ForeignKey('User', verbose_name='Пользователь', related_name='user',
+                                on_delete=models.CASCADE, default=-1)
+    id_user_liked = models.ForeignKey('User', verbose_name='Кто поставил', related_name='user_liked',
+                                      on_delete=models.CASCADE, default=-1)
 
-    def count_likes(self):
-        '''
-        Counts all likes for user
-        :return: likes count
-        '''
-        return UserRating.objects.count()
-
-    def __str__(self):
-        return '%s' % self.count_likes()
+    # def __str__(self):
+    #     return '%s' % self.count_likes()
 
     class Meta:
+        unique_together = ('user', 'id_user_liked')
         verbose_name = 'Рейтинг'
         verbose_name_plural = 'Рейтинги'
 
 
 class User(models.Model):
+    """User Model"""
     username = models.CharField('Имя пользователя', max_length=50)
     password = models.CharField('Пароль', max_length=100)
     email = models.EmailField('E-mail')
@@ -50,16 +49,13 @@ class User(models.Model):
     date_register = models.DateField('Дата регистрации', auto_now_add=True)
     last_online = models.DateTimeField('Заходил последний раз', auto_now_add=True)
     is_blocked = models.BooleanField('Заблокирован', default=False)
-    rating = models.OneToOneField(UserRating, verbose_name='Рейтинг пользователя',
-                                  on_delete=models.SET_NULL, null=True, blank=True)
     coefficient = models.FloatField('Коэффициент', default=0.0)
-    test = models.OneToOneField('Test', verbose_name='Тест', on_delete=models.SET_NULL, null=True, blank=True)
     user_url = models.SlugField(max_length=160, unique=True)
+    test = models.OneToOneField('Test', verbose_name='Тест', on_delete=models.SET_NULL, null=True, blank=True)
     coef_range_min = models.FloatField('Нижняя граница поиска', default=0.0)
     coef_range_max = models.FloatField('Верхняя граница поиска', default=0.0)
     status = models.CharField('Статус', choices=STATUS, max_length=5, null=False)
     display_status = models.BooleanField(default=True)
-
 
     def __str__(self):
         return '%s, %s' % (self.first_name, self.age)
@@ -70,6 +66,7 @@ class User(models.Model):
 
 
 class Answer(models.Model):
+    """Answer Model"""
     answer = models.CharField(max_length=500)
 
     def __str__(self):
@@ -81,6 +78,7 @@ class Answer(models.Model):
 
 
 class Question(models.Model):
+    """Question Model"""
     title = models.CharField(max_length=300)
     description = models.TextField(max_length=5000)
     answers = models.ManyToManyField(Answer, verbose_name='Ответы', default=None, null=True)
@@ -95,6 +93,7 @@ class Question(models.Model):
 
 
 class Test(models.Model):
+    """Test Model"""
     test_title = models.CharField('Название теста', max_length=300)
     test_description = models.TextField('Описание теста', max_length=5000)
     test_questions = models.ManyToManyField(Question, verbose_name='Вопросы', related_name='test_questions')
