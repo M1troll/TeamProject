fd2c516cc699ef65d0d2b0a1cca0e1dd01bc52b7
AndreyNovosyelov <andreynovosyelov2001@mail.ru>
2021-03-23 22:58:26 +0700

    Create frontend branch

---
+++ heartbits/static/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ $('.myheader').height($(window).height());
+ $('.myheader').width($(window).width());
+})
+
+  $(window).resize(function() {
+ $('.myheader').height($(window).height());
+ $('.myheader').width($(window).width());
+  })
